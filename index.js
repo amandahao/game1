0c536242a27f9deba668630858f94ecf5fd81e30
Amanda Hao <amandahao@gmail.com>
2018-04-14 11:47:16 -0700

    fontawesome

--- index.js
+++ index.js
@@ -172,6 +172,76 @@ function startServer() {
 		//res.status(404)
 	});
 
+	/* Defines what function to call when a request comes from the path '/' in http://localhost:8080 */
+	app.get('/login', (req, res, next) => {
+
+		/* Get the absolute path of the html file */
+		var filePath = path.join(__dirname, './login.html')
+
+		/* Sends the html file back to the browser */
+		res.sendFile(filePath);
+		//res.send('whatever')
+		//res.status(404)
+	});
+
+	app.post('/login', (req, res, next) => {
+
+		// Converting the request in an user object
+		var newuser = new usermodel(req.body);
+
+		// Grabbing the password from the request
+		var password = req.body.password;
+
+		// Adding a random string to salt the password with
+		var salt = crypto.randomBytes(128).toString('base64');
+		newuser.salt = salt;
+
+		// Winding up the crypto hashing lock 10000 times
+		var iterations = 10000;
+		crypto.pbkdf2(password, salt, iterations, 256, 'sha256', function(err, hash) {
+			if(err) {
+				return res.send({error: err});
+			}
+			newuser.password = hash.toString('base64');
+			// Saving the user object to the database
+			newuser.save(function(err) {
+
+				// Handling the duplicate key errors from database
+				if(err && err.message.includes('duplicate key error') && err.message.includes('userName')) {
+					return res.send({error: 'Username, ' + req.body.userName + 'already taken'});
+				}
+				if(err) {
+					return res.send({error: err.message});
+				}
+				res.send({error: null});
+			});
+		});
+	});
+
+	/* Defines what function to call when a request comes from the path '/' in http://localhost:8080 */
+	app.get('/login.css', (req, res, next) => {
+
+		/* Get the absolute path of the html file */
+		var filePath = path.join(__dirname, './login.css')
+
+		/* Sends the html file back to the browser */
+		res.sendFile(filePath);
+		//res.send('whatever')
+		//res.status(404)
+	});
+
+	/* Defines what function to call when a request comes from the path '/' in http://localhost:8080 */
+	app.get('/login.js', (req, res, next) => {
+
+		/* Get the absolute path of the html file */
+		var filePath = path.join(__dirname, './login.js')
+
+		/* Sends the html file back to the browser */
+		res.sendFile(filePath);
+		//res.send('whatever')
+		//res.status(404)
+	});
+
 	app.get('/breakfast', (req, res, next) => {
 		res.send('<img src="https://images.unsplash.com/photo-1455853828816-0c301a011711?ixlib=rb-0.3.5&s=f087ed54c63956580923b24bfaa07db7&auto=format&fit=crop&w=668&q=80" />')
 	});
