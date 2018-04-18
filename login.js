b666d0c6fb14306ca653e142b1b202f86dec92e8
Amanda Hao <amandahao@gmail.com>
2018-04-17 19:12:34 -0700

    login

--- login.js
+++ login.js
@@ -1,9 +1,9 @@
-$( ".input" ).focusin(function() {
-  $( this ).find( "span" ).animate({"opacity":"0"}, 200);
+$(".input").focusin(function() {
+  $(this).find("span").animate({"opacity":"0"}, 200);
 });
 
-$( ".input" ).focusout(function() {
-  $( this ).find( "span" ).animate({"opacity":"1"}, 300);
+$(".input").focusout(function() {
+  $(this).find("span").animate({"opacity":"1"}, 300);
 });
 
 $(".login").submit(function(){
